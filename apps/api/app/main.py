23e48417a7c963cddc669cc7eaf384a11b728d31
qiewi <105619444+qiewi@users.noreply.github.com>
2025-05-06 10:03:23 +0700

    chore: init

---
+++ apps/api/app/main.py
@@ -0,0 +1,13 @@
+# apps/api/app/main.py
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello from FastAPI"}
+
+@app.get("/api/items/{item_id}")
+async def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
+

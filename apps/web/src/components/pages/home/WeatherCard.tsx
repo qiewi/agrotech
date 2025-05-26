b3fb2ef9bc4aada358f05b42a6668e9bdbc7d2c7
qiewi <105619444+qiewi@users.noreply.github.com>
2025-05-26 22:42:25 +0700

    fix: text size

--- apps/web/src/components/pages/home/WeatherCard.tsx
+++ apps/web/src/components/pages/home/WeatherCard.tsx
@@ -27,22 +27,22 @@ const WeatherCard = ({
           <Cloud className="text-gray-200 w-16 h-16" />
           <Sun className="text-yellow-400 w-6 h-6 absolute -top-1 -left-1" />
         </div>
-        <h1 className="text-4xl font-bold text-black">{temperature}°C</h1>
+        <h1 className="text-4xl text-black">{temperature}°C</h1>
         <p className="text-black text-sm mt-1">{condition}</p>
       </div>
 
       <div className="bg-white rounded-xl py-4 px-1">
         <div className="grid grid-cols-3 gap-2 text-center">
           <div>
-            <p className="text-lg font-semibold text-black">{humidity}%</p>
+            <p className="text-md font-semibold text-black">{humidity}%</p>
             <p className="text-xs text-gray-500">Humidity</p>
           </div>
           <div>
-            <p className="text-lg font-semibold text-black">{precipitation}</p>
+            <p className="text-md font-semibold text-black">{precipitation}</p>
             <p className="text-xs text-gray-500">Precipitation</p>
           </div>
           <div>
-            <p className="text-lg font-semibold text-black">{windSpeed}</p>
+            <p className="text-md font-semibold text-black">{windSpeed}</p>
             <p className="text-xs text-gray-500">Wind Speed</p>
           </div>
         </div>
